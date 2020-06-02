1c0032fa95ac84509ca1e042a9b7d7b1df001c9e
Kacper <kacper.neumann19@imperial.ac.uk>
2020-06-02 20:09:22 +0100

    Fixed decoder and SM

--- DECODE.v
+++ DECODE.v
@@ -1,7 +1,8 @@
 module DECODE
 (
 	input [15:0] instr,
-	input EXEC,
+	input EXEC1,
+	input EXEC2,
 	input COND_result,
 	output R0_count,
 	output R0_en,
@@ -18,7 +19,9 @@ module DECODE
 	output s4,
 	output RAMd_wren,
 	output RAMd_en,
-	output RAMi_en
+	output RAMi_en,
+	output ALU_en,
+	output E2
 );
 
 	wire msb = instr[15];					//MSB of the instruction word
@@ -35,54 +38,36 @@ module DECODE
 	wire STORE = msb & ls;
 	wire UJMP = ~op[5] & ~op[4] & ~op[3] & ~op[2];
 	wire JMP = (~op[5] & ~op[4] & ~op[3] & op[2]) | (~op[5] & ~op[4] & op[3] & ~op[2]);
-	wire AND = ~op[5] & ~op[4] &  op[3] &  op[2] & ~op[1] & ~op[0];
-	wire OR =  ~op[5] & ~op[4] &  op[3] &  op[2] & ~op[1] &  op[0];
-	wire XOR = ~op[5] & ~op[4] &  op[3] &  op[2] &  op[1] & ~op[0];
-	wire NOT = ~op[5] & ~op[4] &  op[3] &  op[2] &  op[1] &  op[0];
-	wire NND = ~op[5] &  op[4] & ~op[3] & ~op[2] & ~op[1] & ~op[0];
-	wire NOR = ~op[5] &  op[4] & ~op[3] & ~op[2] & ~op[1] &  op[0];
-	wire XNR = ~op[5] &  op[4] & ~op[3] & ~op[2] &  op[1] & ~op[0];
-	wire MOV = ~op[5] &  op[4] & ~op[3] & ~op[2] &  op[1] &  op[0];
-	wire ADD = ~op[5] &  op[4] & ~op[3] &  op[2] & ~op[1] & ~op[0];
-	wire ADC = ~op[5] &  op[4] & ~op[3] &  op[2] & ~op[1] &  op[0];
-	wire ADO = ~op[5] &  op[4] & ~op[3] &  op[2] &  op[1] & ~op[0];
-	wire SUB = ~op[5] &  op[4] &  op[3] & ~op[2] & ~op[1] & ~op[0];
-	wire SBC = ~op[5] &  op[4] &  op[3] & ~op[2] & ~op[1] &  op[0];
-	wire SBO = ~op[5] &  op[4] &  op[3] & ~op[2] &  op[1] & ~op[0];
 	wire MUL = ~op[5] &  op[4] &  op[3] &  op[2] & ~op[1] & ~op[0];
 	wire MLA = ~op[5] &  op[4] &  op[3] &  op[2] & ~op[1] &  op[0];
 	wire MLS = ~op[5] &  op[4] &  op[3] &  op[2] &  op[1] & ~op[0];
-	wire MRT = ~op[5] &  op[4] &  op[3] &  op[2] &  op[1] &  op[0];
-	wire LSL =  op[5] & ~op[4] & ~op[3] & ~op[2] & ~op[1] & ~op[0];
-	wire LSR =  op[5] & ~op[4] & ~op[3] & ~op[2] & ~op[1] &  op[0];
-	wire ASR =  op[5] & ~op[4] & ~op[3] & ~op[2] &  op[1] & ~op[0];
-	wire ROR =  op[5] & ~op[4] & ~op[3] &  op[2] & ~op[1] & ~op[0];
-	wire RRC =  op[5] & ~op[4] & ~op[3] &  op[2] & ~op[1] &  op[0];
 	wire NOP =  op[5] &  op[4] &  op[3] &  op[2] &  op[1] & ~op[0];
 	wire STP =  op[5] &  op[4] &  op[3] &  op[2] &  op[1] &  op[0];
-
-	assign R0_count = EXEC & (~(UJMP | JMP | STP));
-	assign R0_en = EXEC & (~(STORE | NOP | STP) & ~Rd[2] & ~Rd[1] & ~Rd[0] | LOAD & ~Rls[2] & ~Rls[1] & ~Rls[0] | UJMP | JMP & COND_result);
-	assign R1_en = EXEC & (~(UJMP | JMP | STORE | LOAD | NOP | STP) & ~Rd[2] & ~Rd[1] &  Rd[0] | LOAD & ~Rls[2] & ~Rls[1] &  Rls[0]);
-	assign R2_en = EXEC & (~(UJMP | JMP | STORE | LOAD | NOP | STP) & ~Rd[2] &  Rd[1] & ~Rd[0] | LOAD & ~Rls[2] &  Rls[1] & ~Rls[0]);
-	assign R3_en = EXEC & (~(UJMP | JMP | STORE | LOAD | NOP | STP) & ~Rd[2] &  Rd[1] &  Rd[0] | LOAD & ~Rls[2] &  Rls[1] &  Rls[0]);
-	assign R4_en = EXEC & (~(UJMP | JMP | STORE | LOAD | NOP | STP) &  Rd[2] & ~Rd[1] & ~Rd[0] | LOAD &  Rls[2] & ~Rls[1] & ~Rls[0]);
-	assign R5_en = EXEC & (~(UJMP | JMP | STORE | LOAD | NOP | STP) &  Rd[2] & ~Rd[1] &  Rd[0] | LOAD &  Rls[2] & ~Rls[1] &  Rls[0]);
-	assign R6_en = EXEC & (~(UJMP | JMP | STORE | LOAD | NOP | STP) &  Rd[2] &  Rd[1] & ~Rd[0] | LOAD &  Rls[2] &  Rls[1] & ~Rls[0]);
-	assign R7_en = EXEC & (~(UJMP | JMP | STORE | LOAD | NOP | STP) &  Rd[2] &  Rd[1] &  Rd[0] | LOAD &  Rls[2] &  Rls[1] &  Rls[0]);
-	assign s1[2] = EXEC & ((~(UJMP | JMP | STORE | LOAD | NOP | STP) & Rs1[2]) | (STORE & Rls[2]));
-	assign s1[1] = EXEC & ((~(UJMP | JMP | STORE | LOAD | NOP | STP) & Rs1[1]) | (STORE & Rls[1]));
-	assign s1[0] = EXEC & ((~(UJMP | JMP | STORE | LOAD | NOP | STP) & Rs1[0]) | (STORE & Rls[0]));
-	assign s2[2] = EXEC & ((~(UJMP | JMP | STORE | LOAD | NOP | STP)) & Rs2[2]);
-	assign s2[1] = EXEC & ((~(UJMP | JMP | STORE | LOAD | NOP | STP)) & Rs2[1]);
-	assign s2[0] = EXEC & ((~(UJMP | JMP | STORE | LOAD | NOP | STP)) & Rs2[0]);
-	assign s3[2] = EXEC & ((~(UJMP | JMP | STORE | LOAD | NOP | STP)) & Rd[2]);
-	assign s3[1] = EXEC & ((~(UJMP | JMP | STORE | LOAD | NOP | STP)) & Rd[1]);
-	assign s3[0] = EXEC & ((~(UJMP | JMP | STORE | LOAD | NOP | STP)) & Rd[0]);
-	assign s4 = EXEC & ~(LOAD | STORE);
-	assign RAMd_wren = EXEC & STORE;
-	assign RAMd_en = EXEC & (STORE | LOAD);
-	assign RAMi_en = EXEC & ~STP;
+	
+	assign R0_count = EXEC1 & (~(UJMP | JMP | STP));
+	assign R0_en = EXEC1 & (~(STORE | NOP | STP) & ~Rd[2] & ~Rd[1] & ~Rd[0] | UJMP | JMP & COND_result) | EXEC2 & (LOAD | MUL | MLA | MLS) & ~Rls[2] & ~Rls[1] & ~Rls[0];
+	assign R1_en = EXEC1 & ~(UJMP | JMP | STORE | LOAD | MUL | MLA | MLS | NOP | STP) & ~Rd[2] & ~Rd[1] &  Rd[0] | EXEC2 & (LOAD | MUL | MLA | MLS) & ~Rls[2] & ~Rls[1] &  Rls[0];
+	assign R2_en = EXEC1 & ~(UJMP | JMP | STORE | LOAD | MUL | MLA | MLS | NOP | STP) & ~Rd[2] &  Rd[1] & ~Rd[0] | EXEC2 & (LOAD | MUL | MLA | MLS) & ~Rls[2] &  Rls[1] & ~Rls[0];
+	assign R3_en = EXEC1 & ~(UJMP | JMP | STORE | LOAD | MUL | MLA | MLS | NOP | STP) & ~Rd[2] &  Rd[1] &  Rd[0] | EXEC2 & (LOAD | MUL | MLA | MLS) & ~Rls[2] &  Rls[1] &  Rls[0];
+	assign R4_en = EXEC1 & ~(UJMP | JMP | STORE | LOAD | MUL | MLA | MLS | NOP | STP) &  Rd[2] & ~Rd[1] & ~Rd[0] | EXEC2 & (LOAD | MUL | MLA | MLS) &  Rls[2] & ~Rls[1] & ~Rls[0];
+	assign R5_en = EXEC1 & ~(UJMP | JMP | STORE | LOAD | MUL | MLA | MLS | NOP | STP) &  Rd[2] & ~Rd[1] &  Rd[0] | EXEC2 & (LOAD | MUL | MLA | MLS) &  Rls[2] & ~Rls[1] &  Rls[0];
+	assign R6_en = EXEC1 & ~(UJMP | JMP | STORE | LOAD | MUL | MLA | MLS | NOP | STP) &  Rd[2] &  Rd[1] & ~Rd[0] | EXEC2 & (LOAD | MUL | MLA | MLS) &  Rls[2] &  Rls[1] & ~Rls[0];
+	assign R7_en = EXEC1 & ~(UJMP | JMP | STORE | LOAD | MUL | MLA | MLS | NOP | STP) &  Rd[2] &  Rd[1] &  Rd[0] | EXEC2 & (LOAD | MUL | MLA | MLS) &  Rls[2] &  Rls[1] &  Rls[0];
+	assign s1[2] = EXEC1 & ((~(UJMP | JMP | STORE | LOAD | NOP | STP) & Rs1[2]) | (STORE & Rls[2]));
+	assign s1[1] = EXEC1 & ((~(UJMP | JMP | STORE | LOAD | NOP | STP) & Rs1[1]) | (STORE & Rls[1]));
+	assign s1[0] = EXEC1 & ((~(UJMP | JMP | STORE | LOAD | NOP | STP) & Rs1[0]) | (STORE & Rls[0]));
+	assign s2[2] = EXEC1 & ((~(UJMP | JMP | STORE | LOAD | NOP | STP)) & Rs2[2]);
+	assign s2[1] = EXEC1 & ((~(UJMP | JMP | STORE | LOAD | NOP | STP)) & Rs2[1]);
+	assign s2[0] = EXEC1 & ((~(UJMP | JMP | STORE | LOAD | NOP | STP)) & Rs2[0]);
+	assign s3[2] = EXEC1 & ((~(UJMP | JMP | STORE | LOAD | NOP | STP)) & Rd[2]);
+	assign s3[1] = EXEC1 & ((~(UJMP | JMP | STORE | LOAD | NOP | STP)) & Rd[1]);
+	assign s3[0] = EXEC1 & ((~(UJMP | JMP | STORE | LOAD | NOP | STP)) & Rd[0]);
+	assign s4 = EXEC1 & ~(LOAD | STORE);
+	assign RAMd_wren = EXEC1 & STORE;
+	assign RAMd_en = EXEC1 & (STORE | LOAD);
+	assign RAMi_en = EXEC1 & ~STP | EXEC2 & (LOAD | MUL | MLA | MLS);
+	assign ALU_en = LOAD | STORE;
+	assign E2 = EXEC1 & (LOAD | MUL | MLA | MLS);
 	
 endmodule
 	
